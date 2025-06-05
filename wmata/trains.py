14717ab4a6901ea0e56d28fdaf54c4161babec5a
Alex Leedom <aleedom@Mac-Studio.local>
2025-06-04 20:43:17 -0400

    added a get_trains function

--- wmata/trains.py
+++ wmata/trains.py
@@ -15,19 +15,31 @@ STATION_CODE = "D07" #potomac ave
 # we can put this in the function 
 
 
-def get_trains(STATION_CODE = STATION_CODE):
+def get_trains(STATION_CODE = STATION_CODE, line_filter = 'SV'):
     """
     Parses a response object from the WMATA API. 
+    Args: 
+    STATION_CODE: Station code of the station you want. 
+    line: Line. Defaults to 'SV' for Silver Line trains. Enter None or 'any' for the next three trains. 
+    Possible lines are 'OR','BL','SV', 'YL','GR'
     """
-    url = f'http://api.wmata.com/StationPrediction.svc/json/GetPrediction/{STATION_CODE}'
+    url = f'http://api.wmata.com/StationPrediction.svc/json/GetPrediction/{STATION_CODE}' #wmata train prediction url
     headers = {'api_key': API_KEY}
-    response = requests.get(url, headers=headers)
-    data = response.json()
+    line_filter = line_filter.lower()
+    # handle errors 
+    try: 
+        response = requests.get(url, headers=headers, timeout= 5)
+        response.raise_for_status()
+    except requests.RequestException() as e: 
+        print(f'Error fetching train data: {e}')
+    data = response.json().get('Trains', [])
     trains = data['Trains']
-    # filter for silver line trains 
-    trains = [t for t in trains if t["Line"] == 'SV'] # Silver line only 
-    
-
-train_data = get_trains()
+    # filter for silver line trains
+    if line_filter != 'any': 
+        trains = [t for t in trains if t["Line"] == line_filter] # Silver line only 
+    else: 
+        trains = trains[:3] # next three trains 
+    return trains 
 
 def format_train_data(trains): 
+    #TODO 
