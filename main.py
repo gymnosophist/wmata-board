11b222029d98d763fea452bd19313bce6e7cb5f8
Alex Leedom <aleedom@Mac-Studio.local>
2025-06-07 15:01:15 -0400

    edited exception handling, line filtering

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from wmata.trains import get_trains, display_trains 
 import os 
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 API_KEY=os.getenv("WMATA_API_KEY")
-STATION_CODE = "D07" #potomac ave 
 
-url = f'http://api.wmata.com/StationPrediction.svc/json/GetPrediction/{STATION_CODE}'
-headers = {'api_key': API_KEY}
-
-response = requests.get(url, headers=headers)
-data = response.json()
-
-print(data)
+STATION_CODE = 'D07'
+line_filter = 'SV'
+def main(): 
+    trains = get_trains(STATION_CODE=STATION_CODE, line_filter='SV')
+    if trains:
+        print('got trains')
+    else:
+        print('error getting trains')
+    display_trains(trains)
+    
+if __name__ == "__main__":
+    main()
--- wmata/trains.py
+++ wmata/trains.py
@@ -9,7 +9,7 @@ load_dotenv()
 API_KEY=os.getenv("WMATA_API_KEY")
 
 # check for API key 
-assert WMATA_API_KEY, "WMATA_API_KEY not found in environment."
+assert API_KEY, "WMATA_API_KEY not found in environment."
 
 STATION_CODE = "D07" #potomac ave 
 # we can put this in the function 
@@ -25,21 +25,28 @@ def get_trains(STATION_CODE = STATION_CODE, line_filter = 'SV'):
     """
     url = f'http://api.wmata.com/StationPrediction.svc/json/GetPrediction/{STATION_CODE}' #wmata train prediction url
     headers = {'api_key': API_KEY}
-    line_filter = line_filter.lower()
+
     # handle errors 
     try: 
         response = requests.get(url, headers=headers, timeout= 5)
         response.raise_for_status()
-    except requests.RequestException() as e: 
+    except requests.RequestException as e: 
         print(f'Error fetching train data: {e}')
-    data = response.json().get('Trains', [])
-    trains = data['Trains']
+    trains = response.json().get('Trains', [])
     # filter for silver line trains
     if line_filter != 'any': 
         trains = [t for t in trains if t["Line"] == line_filter] # Silver line only 
+        print(trains)
     else: 
         trains = trains[:3] # next three trains 
     return trains 
 
-def format_train_data(trains): 
-    #TODO 
+
+def display_trains(trains): 
+    print("LN  CAR DESTINATION        MIN")
+    for t in trains:
+        line = t["Line"].ljust(2)
+        car = (t["Car"] if t["Car"] else "-").rjust(3)
+        dest = t["Destination"].upper().ljust(18)
+        min_ = t["Min"].rjust(3)
+        print(f"{line} {car} {dest} {min_}")
