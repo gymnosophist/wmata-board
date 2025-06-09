7c570b8d713982b239696434cf9b70961723c839
Alex Leedom <aleedom@Mac-Studio.local>
2025-06-09 13:00:34 -0400

    added first version of render.py

---
+++ wmata/render.py
@@ -0,0 +1,53 @@
+from config import STATION_CODE, WMATA_API_KEY 
+
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, grapics 
+import os 
+
+# matrix configuration 
+def setup_matrix(rows=32, cols=64, chain_length=1, parallel=1): 
+    """
+    Establishes matrix options. 
+    Rows: Number of rows on the matrix.
+    Cols: Number of columns. 
+    Chain: Number of matrices in chain. 
+    Parallel: Number of matrices in parallel. 
+    """
+    options = RGBMatrixOptions()
+    options.rows=rows 
+    options.cols=cols 
+    options.chain_length = chain_length 
+    options.parallel = parallel 
+    options.hardware_mapping = 'adafruit-hat' #using the adafruit hat for this project; could set to 'regular' 
+    return RGBMatrix(options = options)
+
+def render_trains(trains: dict):
+    """
+    Renders a dictionary of trains 
+    """
+    matrix = setup_matrix()
+    canvas = matrix.CreateFrameCanvas() 
+
+    # load font 
+    font = graphics.Font() 
+    font.LoadFont("/home/pi/wmata-led-board/lib/rgbmatrix/fonts/5x8.bdf")  # update path as needed
+    textColor = graphics.Color(255, 255, 0)  # Yellow-ish text to start 
+
+    y_offset = 10 # starting y position 
+
+    canvas.Clear() 
+
+    # render some train data 
+    for train in trains[:3]: # limit to three trains for now 
+        line = train['Line']
+        car = train['Car']
+        dest = train['Destination']
+        mins = train['Min']
+
+        text = f"{line:<2} {car:<1} {dest:<6} {mins:>2}"
+        graphics.DrawText(canvas, font, 1, y_offset, textColor, text[:11])  # Clip to ~11 chars
+        y_offset += 10
+
+    matrix.SwapOnVSync(canvas)
+
+
+
