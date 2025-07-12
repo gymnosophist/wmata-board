d79485f7970e221073e94396dcbe2a22147c3077
Alex Leedom <aleedom@Mac-Studio.local>
2025-07-12 14:30:06 -0400

    fixed typos

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main():
 
     parser.add_argument(
         "-t", "--test",
-        action="store_true"
+        action="store_true",
         help = "Print train data to console without rendering"
 
     )
